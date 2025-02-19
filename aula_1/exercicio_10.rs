9bd7ba8f919cce1350496d4f4b0008a826144b0c
Renan Gregório <145120433+RenanGreg@users.noreply.github.com>
2025-02-19 10:07:18 -0300

    Create exercicio_10.rs

---
+++ aula_1/exercicio_10.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let vetor1 = vec![1, 2, 3];
+    let vetor2 = vec![4, 5, 6];
+    let combinado = [&vetor1[..], &vetor2[..]].concat();
+
+    println!("{:?}", combinado); // imprimi juntando os dois vetores [1, 2, 3, 4, 5, 6]
+}
