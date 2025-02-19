83b894f9ab7e162775745d73f2b1d1170b1c4499
Renan Gregório <145120433+RenanGreg@users.noreply.github.com>
2025-02-19 09:58:45 -0300

    Create exercicio_3.rs

---
+++ aula_1/exercicio_3.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let frutas = vec!["maçã", "banana", "laranja"];
+
+    let primeira = frutas[0];
+    let ultima = frutas[frutas.len() - 1];
+
+    println!("Primeira fruta: {}", primeira);
+    println!("Última fruta: {}", ultima);
+}
