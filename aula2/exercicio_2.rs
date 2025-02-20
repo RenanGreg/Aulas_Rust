680469d55a9c52c54777abb28cd10de33fd7b453
Renan Gregório <145120433+RenanGreg@users.noreply.github.com>
2025-02-20 08:21:46 -0300

    Create exercicio_2.rs

---
+++ aula2/exercicio_2.rs
@@ -0,0 +1,14 @@
+fn main(){
+    
+    let pessoa = ("Ana", 28, 1.65, true); 
+    
+    let nome = pessoa.0; 
+    
+    let idade = pessoa.1; 
+    
+    let altura = pessoa.2; 
+    
+    let ativo = pessoa.3; 
+    
+    println!("Nome: {}, Idade: {}, Altura: {}", nome, idade, altura);  
+}
