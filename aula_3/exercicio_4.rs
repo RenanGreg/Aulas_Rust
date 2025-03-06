b0e18fb1a6e42f83f25b7e7b1038fa974c34f59f
Renan Gregório <145120433+RenanGreg@users.noreply.github.com>
2025-03-06 09:14:35 -0300

    Create exercicio_4.rs

---
+++ aula_3/exercicio_4.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut lista = vec![1,2,3,4];
+    
+    lista.push(5); // adiciona no final
+    
+    lista.insert(2,10); // adiciona na posição 2 
+    
+    println!("{:?}",lista);
+} 
